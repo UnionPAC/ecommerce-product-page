b427c2d9a510d88f3a13b11008a397502e74d4cd
Geoff Jamieson <geoff.ijamieson@gmail.com>
2024-02-06 13:29:57 -0500

    complete project

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const navMenu: HTMLElement | null = document.querySelector(".nav_menu");
+
+// nav menu functionality
+navMenu?.addEventListener("click", () => {
+  // add class of show to <nav>
+});
